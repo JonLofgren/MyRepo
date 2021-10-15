d0be5c0238d1ffa39b598109375ba715845ea04c
JonLofgren <89050099+JonLofgren@users.noreply.github.com>
2021-10-15 17:03:43 -0400

    Add files via upload

---
+++ Python Code/sorting.py
@@ -0,0 +1,48 @@
+from random import randint, shuffle
+from time import sleep, time
+import os
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def display(arr):
+    clear()
+    for i in range(len(arr)):
+        temp = ["-" for _ in range(arr[i])]
+        for j in temp:
+            print(j, end="")
+        print()
+
+
+def sort(arr):
+    display(arr)
+    for i in range(len(arr)):
+        start = i + 1
+        for j in range(start, len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+                display(arr)
+
+
+def main():
+    clear()
+    arr = []
+    while True:
+        try:
+            len = int(input("How many items do you want to sort?\n"))
+            break
+        except ValueError:
+            print("Please input a valid number!")
+    for i in range(len):
+        arr.append(i + 1)
+    shuffle(arr)
+    clear()
+    start = time()
+    sort(arr)
+    print(f"\nTime to complete: {time() - start} seconds")
+
+
+if __name__ == "__main__":
+    main()

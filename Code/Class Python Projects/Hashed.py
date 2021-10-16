cd3c5007d7da07fb13f917897e6b55e44404d5f8
JonLofgren <89050099+JonLofgren@users.noreply.github.com>
2021-10-15 20:48:26 -0400

    Delete Code/Class Python Projects directory

--- Code/Class Python Projects/Hashed.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-hashed = hashlib.md5(input("What do you want to hash? ").encode("UTF-8")).hexdigest()
-with open("hashed.txt", "w") as file:
-    file.write(f"{hashed}\n")
--- Code/Class Python Projects/shuffle.py
+++
@@ -1,9 +0,0 @@
-from random import shuffle as shuf
-import os
-
-os.system("cls" if os.name == "nt" else "clear")
-
-my_list = ["Apple", "Banana", "Cherry", "Date", "Fig", "Gundam"]
-print(my_list)
-shuf(my_list)
-print(my_list)
